ae9bd90749b2f6e9cfdc4db82f77d82c1ab7892a
Haseeb Asir <haseebasir912@gmail.com>
2026-04-30 23:02:03 +0500

    connection url for postgresql added

--- database/schema.py
+++ database/schema.py
@@ -2,9 +2,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model import Base
-
-DATABASE_URL = 'postgresql://username:admin123@localhost:5432/bidding_app'
+# database connection string for postgreSQL
+DATABASE_URL = 'postgresql://postgres:admin123@localhost:5432/bidding_app'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
+
+# pydantic models for data validation
+from pydantic import BaseModel
